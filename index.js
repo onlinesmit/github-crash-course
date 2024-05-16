df13b236c13ac009ac79a1382ae26b02739e8809
smit kumar <onlinesmitg@gmail.com>
2024-05-16 19:48:50 +0530

    Added above 18 fun

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const age = 25;
-const name = "Smit k";
+const name1 = "Smit k";
 const dob = '17/09/1999';
 
 function sayName() {
-    console.log("My name is: " +name);
+    console.log("My name is: " +name1);
+}
+
+
+function isAbove(age) {
+    return age >=18;
 }
